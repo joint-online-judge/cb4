9246bd18f663b8204add7564350d7e79cfc8d2e5
tc-imba <liuyh615@sjtu.edu.cn>
2018-07-09 01:59:50 +0800

    feature: multiple select code languages (with select2)

--- vj4/ui/components/form/select.page.js
+++ vj4/ui/components/form/select.page.js
@@ -1,8 +1,9 @@
 import { AutoloadPage } from 'vj/misc/PageLoader';
 import 'select2';
+import 'select2/dist/css/select2.css';
 
 const selectPage = new AutoloadPage('selectPage', () => {
-  console.log(1);
+  $('.select.select2').select2();
 });
 
 export default selectPage;
