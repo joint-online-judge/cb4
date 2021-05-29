6bbb19c9df3e2f7972026b616b24c3f2d73c1499
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-29 17:55:27 +0800

    fix: run_in_executor moss.send

--- vj4/model/adaptor/moss.py
+++ vj4/model/adaptor/moss.py
@@ -6,6 +6,7 @@ from mosspy import Moss
 from io import BytesIO
 
 import rarfile, tarfile, zipfile
+import asyncio
 
 from vj4 import constant
 from vj4 import db
@@ -80,7 +81,7 @@ async def moss_test(rdocs: list, language: str, ignore_limit: int = 10, wildcard
                 except Exception as e:
                     print(e)
 
-        url = moss.send()
+        url = await asyncio.get_running_loop().run_in_executor(None, moss.send)
         return url
 
     finally:
